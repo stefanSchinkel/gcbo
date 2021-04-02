1fc57eb5be7ca52369ea1246e1cdce3aedc548e4
Stefan Schinkel <mail@dreeg.org>
2021-04-02 20:25:42 +0200

    [ save-repo] basic saving

--- test/test_repomgmr.py
+++ test/test_repomgmr.py
@@ -1,6 +1,7 @@
 """Test for repo manager"""
 
 import os
+import json
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader
 
@@ -23,3 +24,19 @@ def test_with_no_cfg(tmp_path):
 
     # Assert
     assert rm.has_cfg is False
+
+def test_add_repo_to_tempyt(tmpdir):
+    """Test default new start"""
+    # Arange
+    cfg = tmpdir.mkdir("sub").join("empty.json")
+    new_repo = "foo.bar/foobar"
+
+    # Act
+    rm = gcbo.RepoManager(repo="foo.bar/baz", cfg=cfg)
+    rm.save("foorepo")
+
+    # Assert
+    with open(cfg) as fp:
+        data = json.load(cfg)
+    print(data)
+    assert 1 == 1
